0792865393b53a69c28be2a442a9bc644be27bbc
Rawan Marzouq <rawan.marzouq@gmail.com>
2018-07-26 10:50:21 +0300

    First Version of RateItControl

--- RateItControl.podspec
+++ RateItControl.podspec
@@ -9,7 +9,7 @@
 Pod::Spec.new do |s|
   s.name             = 'RateItControl'
   s.version          = '0.1.0'
-  s.summary          = 'A short description of RateItControl.'
+  s.summary          = 'iOS Swift ccustom control for rating.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
@@ -18,7 +18,7 @@ Pod::Spec.new do |s|
 #   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+iOS Swift 4.1 custom control for rating, all you need is to add it to your project!
                        DESC
 
   s.homepage         = 'https://github.com/rawanmatar89/RateItControl'
@@ -31,12 +31,14 @@ TODO: Add long description of the pod here.
   s.ios.deployment_target = '8.0'
 
   s.source_files = 'RateItControl/Classes/**/*'
+  s.resources = 'RateItControl/Assets/**/*'
   
   # s.resource_bundles = {
   #   'RateItControl' => ['RateItControl/Assets/*.png']
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+  s.frameworks = 'UIKit'
   # s.dependency 'AFNetworking', '~> 2.3'
+  s.swift_version = '4.1'
 end
--- RateItControl/Classes/RateItControl.swift
+++ RateItControl/Classes/RateItControl.swift
@@ -6,3 +6,128 @@
 //
 
 import Foundation
+import UIKit
+
+@available(iOS 9.0, *)
+@IBDesignable public class RatingControl: UIStackView{
+    
+    //MARK: Inspectable Properties
+    @IBInspectable public var elementsCount : Int = 5{
+        didSet{
+            setupElements()
+        }
+    }
+    @IBInspectable public var rating : Int = 3{
+        didSet{
+            updateButtonSelectionStates()
+        }
+    }
+    @IBInspectable public var filledImage : UIImage?{
+        didSet{
+            setupElements()
+        }
+    }
+    @IBInspectable public var emptyImage : UIImage?{
+        didSet{
+            setupElements()
+        }
+    }
+    @IBInspectable public var elementSize : CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet{
+            setupElements()
+        }
+    }
+    //MARK: Properties
+    public var ratingElements = [UIButton]()
+    
+    //MARK: Initialization
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        // Check element image
+        checkImages()
+        
+        // Setup elements
+        setupElements()
+    }
+    required public init(coder: NSCoder) {
+        super.init(coder:coder)
+        
+        // Check element image
+        checkImages()
+        
+        // Setup elements
+        setupElements()
+    }
+    
+    public func checkImages(){
+        let bundle = Bundle(for: type(of: self))
+        if filledImage == nil {
+            filledImage = UIImage(named: "Filled", in: bundle, compatibleWith: self.traitCollection)
+        }
+        if emptyImage == nil {
+            emptyImage = UIImage(named: "Empty", in: bundle, compatibleWith: self.traitCollection)
+        }
+    }
+    //MARK: Create rating elements
+    private func setupElements(){
+        
+        // Clear all existing buttons
+        for button in ratingElements{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingElements.removeAll()
+        
+        // Create new buttons
+        for i in 0..<elementsCount {
+            
+            // Create button
+            let button = UIButton()
+            button.tag = i
+            
+            // Add constraint
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: elementSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: elementSize.width).isActive = true
+            
+            
+            // Set button image
+            button.setImage(emptyImage, for: .normal)
+            button.setImage(filledImage, for: .highlighted)
+            button.setImage(filledImage, for: .selected)
+            
+            // Setup button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            // Add button to the stack view
+            addArrangedSubview(button)
+            
+            // Add button to rating elements array
+            ratingElements.append(button)
+        }
+        updateButtonSelectionStates()
+    }
+    
+    //MARK: Button actions
+    @objc func ratingButtonTapped(button: UIButton){
+        print(button.tag)
+        guard let index = ratingElements.index(of: button)else{
+            fatalError("The button, \(button), is not in the ratingElements array: \(ratingElements)")
+        }
+        let selectedRating = index + 1
+        if selectedRating == rating {
+            rating = 0
+        }
+        else{
+            rating = selectedRating
+        }
+        
+    }
+    
+    private func updateButtonSelectionStates(){
+        for (index, button) in ratingElements.enumerated() {
+            button.isSelected = index < rating
+        }
+    }
+}
